5daad005eee63f9c6dbdd7b0dcf865d38d76291a
Lia OBrien <liaobrien123@gmail.com>
2021-09-13 21:37:26 -0400

    modified question objects and created function to call first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,59 +4,107 @@ var timeLeft = 75;
 var timer = document.getElementById("timer")
 
 
-var changeQuestionNum = document.getElementById("question-number")
+var getContainer = document.querySelector(".quizbox")
 
 var changeQuestion = document.getElementById("question")
 
+var answerChoices = document.createElement("ul")
+var a = document.createElement("li");
+var b = document.createElement("li");
+var c = document.createElement("li");
+var d = document.createElement("li");
+
 // put questions in objects within an array
-var questions = [
+var allQuestions = [
       {
-            q: "Commonly used data type Do Not include:---",
-            choices: ["strings", "booleance", "alerts", "numbers"],
-            answer: "alerts"
+            question: "Commonly used data type Do Not include: ---",
+            choices: {
+                  a: "strings",
+                  b: "alerts",
+                  c: "booleans",
+                  d: "numbers"
+            },
+            answer: "b"
       },
       {
-            q: "The condition in an if/else statement is enclosed within:---",
-            choices: ["quotes", "Curly brackets", "parentheses", "square brackets"],
-            answer: "parentheses"
+            question: "The condition in an if/else statement is enclosed within:---",
+            choices: {
+                  a: "quotes",
+                  b: "parentheses",
+                  c: "curly brackets",
+                  d: "square brackets"
+            },
+            answer: "b"
       },
       {
-            q: "Arrays in JavaScript can be used to store:---",
-            choices: ["numbers and strings", "other Arrays", "booleances", "all of the above"],
-            answer: "all of the above"
+            question: "Arrays in JavaScript can be used to store:---",
+            choices: {
+                  a: "numbers and strings",
+                  b: "other arrays",
+                  c: "booleans",
+                  d: "all of the above"
+            },
+            answer: "d"
       },
       {
-            q: "String values must be enclosed within --- when being assigned to variables ",
-            choices: ["commas", "curly brackets", "quotes", "parentheses"],
-            answer: "quotes"
+            question: "String values must be enclosed within --- when being assigned to variables ",
+            choices: {
+                  a: "commas",
+                  b: "curly brackets",
+                  c: "quotes",
+                  d: "parentheses"
+            },
+            answer: "c"
       },
       {
-            q: "A very useful tool used during development and debugging for printing content to the debugger is:---",
-            choices: ["JavaScript", "terminal/bash", "alerts", "console.log"],
-            answer: "console.log"
+            question: "A very useful tool used during development and debugging for printing content to the debugger is:---",
+            choices: {
+                  a: "console.log",
+                  b: "JavaScript",
+                  c: "terminal/bash",
+                  d: "alerts"
+            },
+            answer: "a"
       },
 ]
 
 var startButton = document.getElementById("start");
 
-startButton.addEventListener("click", function () {
+
+function startTimer() {
+      startButton.setAttribute("style", "display: none");
       var timeInterval = setInterval(function () {
             if (timeLeft > 0) {
                   timer.textContent = "Time: " + timeLeft;
             }
             else if (timeLeft === 0) {
-                  timer.textContent = "Time: " + timeLeft;
+                  timer.textContent = "Time's up!";
                   clearInterval(timeInterval);
                   // call a function that shows game over or skip to entering score page?
             }
 
             timeLeft--;
+
       }, 1000)
-});
+      showQuestions();
+}
 
+function showQuestions() {
+      changeQuestion.innerHTML = allQuestions[0].question;
 
+      getContainer.appendChild(answerChoices);
+      answerChoices.appendChild(a);
+      answerChoices.appendChild(b);
+      answerChoices.appendChild(c);
+      answerChoices.appendChild(d);
 
+      a.textContent = allQuestions[0].choices.a;
+      b.textContent = allQuestions[0].choices.b;
+      c.textContent = allQuestions[0].choices.c;
+      d.textContent = allQuestions[0].choices.d;
+}
 
+startButton.addEventListener("click", startTimer);
 
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
